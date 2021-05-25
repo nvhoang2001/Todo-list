dc4442640765ac5421ea2d0b2ab4cecdb1588c93
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-05-25 18:42:06 +0700

    cache busting, add HTML-Webpack-Plugin to webpack and add template file to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "development",
 	entry: "./src/scripts/main.js",
 	output: {
-		filename: "main.js",
+		filename: "main-[contenthash].js",
 		path: path.resolve(__dirname, "asserts", "scripts"),
 	},
 	devtool: "inline-source-map",
@@ -20,4 +21,10 @@ module.exports = {
 			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			filename: "../../index.html",
+		}),
+	],
 };
